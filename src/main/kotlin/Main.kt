6669f84016bdd0a00bf3f46ae54ce786fb2839bd
Valentin Volobuev <vvolobuev@okko.tv>
2025-09-28 20:28:45 +0300

    Homework for Kotlin Lesson "Generics and collections"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -15,9 +15,6 @@ fun main() {
     val firstPost = Post(1, 1, "text", 1, 1, 1, 1, "post", "text", 1, true, true, true, true, true, likes = Likes(0,true,true,true), arrayOfAttachments, comments)
     val secondPost = Post(2, 2, "text", 2, 2, 2, 2, "post", "text", 2, true, true, true, true, true, likes = Likes(0,true,true,true),arrayOfAttachments, comments)
 
-    val firstNote = Note("1","","1",1,1,1,"1",1,"1",1,"1",false)
-    val secondNote = Note("1","","1",1,1,1,"1",1,"1",1,"1",false)
-
     WallService.add(firstPost)
     WallService.add(secondPost)
     WallService.update(firstPost)
